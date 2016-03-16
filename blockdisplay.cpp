bfb6a0b5032a4be28c614f754faddfc953bf9eff
Ryan Ragle <rtragle@gmail.com>
2016-03-16 13:19:39 -0700

    commit more from osx machine

--- blockdisplay.cpp
+++ blockdisplay.cpp
@@ -1,7 +1,29 @@
 #include "blockdisplay.h"
 
-BlockDisplay::BlockDisplay()
-{
 
+BlockDisplay::BlockDisplay(QObject *parent) {
+
+}
+
+BlockDisplay::BlockDisplay(int blocknum, long time, QObject *parent) : BlockDisplay(parent) {
+    this->m_blocknumber = blocknum;
+    this->m_time = time;
+}
+
+BlockDisplay::BlockDisplay(const BlockDisplay &other, QObject *parent) : BlockDisplay(parent) {
+    this->m_blocknumber = other.blockNumber();
+    this->m_time = other.time();
+}
+
+BlockDisplay::~BlockDisplay() {
+
+}
+
+int BlockDisplay::blockNumber() const {
+    return this->m_blocknumber;
+}
+
+long BlockDisplay::time() const {
+    return this->m_time;
 }
 
--- blockdisplay.h
+++ blockdisplay.h
@@ -1,11 +1,27 @@
 #ifndef BLOCKDISPLAY_H
 #define BLOCKDISPLAY_H
 
+#include <QObject>
+#include <QMetaType>
 
-class BlockDisplay
+class BlockDisplay : public QObject
 {
+    Q_OBJECT
+
 public:
-    BlockDisplay();
+    BlockDisplay(QObject *parent = 0);
+    BlockDisplay(int blocknum, long time, QObject *parent = 0);
+    BlockDisplay(const BlockDisplay &other, QObject *parent = 0);
+    ~BlockDisplay();
+
+    int blockNumber() const;
+    long time() const;
+
+private:
+    int m_blocknumber;
+    long m_time;
 };
+
+Q_DECLARE_METATYPE(BlockDisplay)
 
 #endif // BLOCKDISPLAY_H
