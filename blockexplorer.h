2fdfa5a464b7af06a22b317c5890d3cf7fbb4176
jaybny <jaybny@gmail.com>
2016-04-29 10:38:44 -0700

    the blockexplorer class for interop

---
+++ blockexplorer.h
@@ -0,0 +1,56 @@
+#ifndef BLOCKEXPLORER
+#define BLOCKEXPLORER
+#include <string>
+#include <createmeta.h>
+#include <ldbwriter.h>
+
+using namespace std;
+namespace fantasybit_bx {
+
+
+class BlockExplorer {
+public:
+
+    BlockExplorer() : cm(ldb) {
+        prev = "";
+    }
+
+    CreateMeta cm;
+    std::string prev;
+    int height;
+    int start;
+    LdbWriter ldb;
+
+    int init(const std::string &startread) {
+        BlockMeta bm;
+
+        auto headid = ldb.read(startread);
+        ldb.read(headid,bm);
+
+        start = bm.blocknum();
+        prev = headid;
+        return start;
+    }
+
+    int init(int start) {
+        string sread = "blocknum_"+ to_string(start);
+        return init(sread);
+    }
+
+    int init() {
+        return init("blockhead");
+    }
+
+    void pblock(const fantasybit_bx::Block &b) {
+        prev = cm.DoMeta(b,prev);
+        ldb.write("blocknum_" + to_string(b.signedhead().head().num()),prev);
+    }
+
+    void endit() {
+        if ( prev != "")
+            ldb.write("blockhead",prev);
+    }
+};
+}
+#endif // BLOCKEXPLORER
+
